0e3902b330ddce4e04ee6698643592b86b92f857
Florian Schaessens <f.schaessens@gmail.com>
2024-09-29 11:45:22 +0200

    add introduction

--- src/components/Exercice1.jsx
+++ src/components/Exercice1.jsx
@@ -1,4 +1,5 @@
 import Exercice from "./container/Exercice";
+import { useState } from "react";
 
 export default function Exercice1() {
 // 1. Utilisez un state pour stocker la valeur saisie par l'utilisateur.
@@ -7,15 +8,16 @@ export default function Exercice1() {
 // 4. Utilisez `useEffect` pour récupérer la valeur initiale du localStorage avec `localStorage.getItem` lors du premier rendu 
 //    et mettre à jour l'état avec cette valeur.
 
+
   return (
     <Exercice>
-      <h2>Stocker le texte dans LocalStorage</h2>
+      <h2>Excercie 1 : Stocker le texte dans LocalStorage</h2>
       <p className="instructions">
       Objectif : Chaque fois que l'utilisateur tape dans le champ de texte, la valeur doit être sauvegardée dans le LocalStorage. Lorsque l'utilisateur recharge la page, la valeur doit être restaurée à partir du LocalStorage.
       </p>
       <div className="solution">
         <div>
-          <input type="test" />
+          <input type="text" />
         </div>
       </div>
     </Exercice>
