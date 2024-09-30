38554092e9880bca9e1bc4e3ab1edfc30a6c3aba
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-09-30 19:48:58 +0200

    on going

--- src/components/Exercice2.jsx
+++ src/components/Exercice2.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Exercice from "./container/Exercice";
 
 export default function Exercice2() {
   // 1. Créez une fonction qui récupère une valeur sauvegardée dans localStorage.
   // 2. Utilisez `localStorage.getItem` pour récupérer cette valeur.
   // 3. Affichez cette valeur.
+
+  const [count, setCount] = useState(getSavedCount)
+  const handleAddClick = () => {
+    setCount(count + 1)
+  }
+  
+  const handleSubstractClick = () => {
+    setCount(count - 1)
+  }
+
+  const getSavedCount = () => {
+    const storedCount = localStorage.getItem('count')
+    if (storedCount) {
+      return parseInt(storedCount)
+    } else {
+      return 0;
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("count", count)
+  }, [count])
 
   return (
     <Exercice>
@@ -15,9 +38,9 @@ export default function Exercice2() {
       </p>
       <div className="solution">
         <div>
-          <button>-</button>
-          <button>+</button>
-          <p>Compteur : 0</p>
+          <button onClick={() => handleSubstractClick()}>-</button>
+          <button onClick={() => handleAddClick()}>+</button>
+          <p>Compteur : {count}</p>
         </div>
       </div>
     </Exercice>
