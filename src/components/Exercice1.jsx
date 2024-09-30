498e87380bdc7ffa9303878b879e9670e54a202a
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-09-30 19:26:08 +0200

    exercice 1 ok

--- src/components/Exercice1.jsx
+++ src/components/Exercice1.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import Exercice from "./container/Exercice";
 
 export default function Exercice1() {
   // 1. Utilisez un state pour stocker la valeur saisie par l'utilisateur.
+  const [text, setText] = useState("")
   // 2. Utilisez une fonction qui sera appelée lorsqu'une valeur est saisie pour mettre à jour l'état avec la valeur du champ de saisie.
   // 3. Utilisez `localStorage.setItem` pour enregistrer la valeur dans localStorage lorsque l'utilisateur saisit une nouvelle valeur.
+  const handleChange = (e) => {
+    const newText = e.target.value;
+    setText(newText)
+    localStorage.setItem('salut', newText)
+  }
   // 4. Utilisez `useEffect` pour récupérer la valeur initiale du localStorage avec `localStorage.getItem` lors du premier rendu
   //    et mettre à jour l'état avec cette valeur.
+  useEffect(() => {
+    const StoredText = localStorage.getItem('salut')
+    setText(StoredText)
+  }, [])
 
   return (
     <Exercice>
@@ -18,7 +29,7 @@ export default function Exercice1() {
       </p>
       <div className="solution">
         <div>
-          <input type="text" />
+          <input type="text" value={text} onChange={handleChange}/>
         </div>
       </div>
     </Exercice>
